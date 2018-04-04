d5d2687f936d8a7ae2e89c0021ba6305ee96d7bf
sandybemonkey <sandy.pierre@bemonkey.net>
2018-04-04 13:22:48 +0200

    Modify Resrvation form to add user id and initial state, Add workflow guards on transitions

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,7 +12,6 @@ class IndexController extends Controller
      */
     public function index()
     {
-
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
