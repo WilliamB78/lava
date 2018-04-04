fe93398b6f4a0131de91c9688774f3d69a20c601
Alexis coubard <couby72220@gmail.com>
2018-04-04 10:25:53 +0200

    Entity&Repository : Parametrage

---
+++ src/Entity/Parametrage.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ParametrageRepository")
+ */
+class Parametrage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=false, unique=true)
+     */
+    private $key;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $value;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param mixed $key
+     */
+    public function setKey($key): void
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value): void
+    {
+        $this->value = $value;
+    }
+}
