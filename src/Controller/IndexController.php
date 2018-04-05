0eddb8f665f7df1fc1566f1b7add4979c24bd70a
Alexis coubard <couby72220@gmail.com>
2018-04-05 12:35:26 +0200

    Ajout des badges dans les menu

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use App\Entity\Room;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,5 +17,40 @@ class IndexController extends Controller
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
+    }
+
+    public function navUser()
+    {
+        $mesReservations = $this
+            ->getDoctrine()
+            ->getRepository(Reservation::class)
+            ->findUserReversationByState($this->getUser(),'accepted');
+
+        return $this->render('navbar/navbar-user.html.twig',[
+            'mesReservations' => $mesReservations
+        ]);
+    }
+
+    public function navSecretary ()
+    {
+        $countHS = $this
+            ->getDoctrine()
+            ->getRepository(Room::class)
+            ->findTotalRoomHS();
+
+        $demReservation = $this
+            ->getDoctrine()
+            ->getRepository(Reservation::class)
+            ->findByState('created');
+
+        return $this->render('navbar/navbar-secretary.html.twig', [
+            'countHS' => $countHS,
+            'demReservation' => $demReservation
+        ]);
+    }
+
+    public function navAdmin ()
+    {
+        return $this->render('navbar/navbar-admin.html.twig');
     }
 }
