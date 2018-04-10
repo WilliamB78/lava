da40648b95edc8535c180a802e4161b100b8102e
Alexis coubard <couby72220@gmail.com>
2018-04-10 12:42:03 +0200

    Test fonctionnelle index, reservation, room

---
+++ tests/Controller/ReservationControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coubardalexis
+ * Date: 09/04/2018
+ * Time: 14:18
+ */
+
+namespace App\Tests\Controller;
+
+
+class ReservationControllerTest
+{
+
+}
