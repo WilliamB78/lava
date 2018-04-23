ec769d3204635bb98c323b4886ec25c6dd0eef7a
Sandy PIERRE <sandy.pierre@bemonkey.net>
2018-04-23 14:49:01 +0200

    Set all role for each users on prepersist and preupdate

--- src/EvenSuscriber/EncryptedPasswordSuscriber.php
+++ src/EvenSuscriber/EncryptedPasswordSuscriber.php
@@ -35,6 +35,16 @@ class EncryptedPasswordSuscriber implements EventSubscriber
         }
         $this->encodeUserPassword($entity);
 
+        if ($entity->getRoles()[0] == "ROLE_UTILISATEUR") {
+            $this->addRole($entity);
+        }
+        if ($entity->getRoles()[0] == "ROLE_SECRETARY") {
+            $this->addSecretaryRole($entity);
+        }
+        if ($entity->getRoles()[0] == "ROLE_ADMIN") {
+            $this->addAdminRole($entity);
+        }
+
     }
 
     public function preUpdate(LifecycleEventArgs $args)
@@ -61,6 +71,30 @@ class EncryptedPasswordSuscriber implements EventSubscriber
             $user->getPassword()
         );
         $user->setPassword($encoded);
+    }
+
+    /**
+     * @param User $user
+     */
+    public function addUserRole(User $user){
+        $user->addRole("ROLE_CAN_DO_BOOKING");
+        $user->addRole("ROLE_CAN_SEE_CALENDAR");
+    }
+
+    /**
+     * @param User $user
+     */
+    public function addSecretaryRole(User $user){
+        $user->addRole("ROLE_CAN_EDIT_ROOM");
+        $user->addRole("ROLE_CAN_SEE_CALENDAR");
+    }
+
+    /**
+     * @param User $user
+     */
+    public function addAdminRole(User $user){
+        $user->addRole("ROLE_CAN_ADD_ROOM");
+        $user->addRole("ROLE_CAN_REMOVE_ROOM");
     }
 
     /**
